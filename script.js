6108f0d5bb8432bbb42c77dd9fcb6be6637a01fe
viktorritsvall <60578044+viktorritsvall@users.noreply.github.com>
2020-04-06 16:19:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,57 +1,61 @@
-
 document.addEventListener("DOMContentLoaded", function modal(){
-    // li in Hall of Books
-    let first = document.getElementById("first");
-    let second = document.getElementById("second");
-    let third = document.getElementById("third");
-  
-    // close the modal
-    var closeModalFirst = document.getElementsByClassName("close-one")[0];
-    var closeModalSecond = document.getElementsByClassName("close-two")[0];
-    var closeModalThird = document.getElementsByClassName("close-three")[0];
-  
-    // modal div 
-    var firstModal = document.getElementById("first-modal");
-    var secondModal = document.getElementById("second-modal");
-    var thirdModal = document.getElementById("third-modal");
-  
-  
-    // first Modal
-    first.onclick = function(event) {
-        firstModal.style.display = "block";
+  // li in Hall of Books
+  let first = document.getElementById("first");
+  let second = document.getElementById("second");
+  let third = document.getElementById("third");
+
+  // close the modal
+  var closeModalFirst = document.getElementsByClassName("close-one")[0];
+  var closeModalSecond = document.getElementsByClassName("close-two")[0];
+  var closeModalThird = document.getElementsByClassName("close-three")[0];
+
+  // modal div 
+  var firstModal = document.getElementById("first-modal");
+  var secondModal = document.getElementById("second-modal");
+  var thirdModal = document.getElementById("third-modal");
+
+
+  // first Modal
+  first.onclick = function(event) {
+      firstModal.style.display = "block";
+    }
+  closeModalFirst.onclick = function(event) {
+    firstModal.style.display = "none";
+  }
+  window.onclick = function(event) {
+      if (event.target === firstModal) {
+        firstModal.style.display = "none";
       }
-    closeModalFirst.onclick = function(event) {
-      firstModal.style.display = "none";
     }
-    window.onclick = function(event) {
-        if (event.target === firstModal) {
-          firstModal.style.display = "none";
-        }
+
+  // Second Modal
+  second.onclick = function(event) {
+    secondModal.style.display = "block";
+  }
+  closeModalSecond.onclick = function() {
+    secondModal.style.display = "none";
+  }
+  window.onclick = function(event) {
+      if (event.target === secondModal) {
+        secondModal.style.display = "none";
       }
+    }
+
+  // Third Modal
+  third.onclick = function(event) {
+    thirdModal.style.display = "block";
+  }
+  closeModalThird.onclick = function() {
+    thirdModal.style.display = "none";
+  }
+  window.onclick = function(event) {
+      if (event.target === thirdModal) {
+        thridModal.style.display = "none";
+      }
+    }
+
+
+
+  // Quiz
   
-    // Second Modal
-    second.onclick = function(event) {
-      secondModal.style.display = "block";
-    }
-    closeModalSecond.onclick = function() {
-      secondModal.style.display = "none";
-    }
-    window.onclick = function(event) {
-        if (event.target === secondModal) {
-          secondModal.style.display = "none";
-        }
-      }
-  
-    // Third Modal
-    third.onclick = function(event) {
-      thirdModal.style.display = "block";
-    }
-    closeModalThird.onclick = function() {
-      thirdModal.style.display = "none";
-    }
-    window.onclick = function(event) {
-        if (event.target === thirdModal) {
-          thridModal.style.display = "none";
-        }
-      }
-  });
+});
