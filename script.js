00d7bfca40ce909be6640ea0f42ec73e8329ac21
viktorritsvall <60578044+viktorritsvall@users.noreply.github.com>
2020-04-07 22:57:49 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -53,9 +53,64 @@ document.addEventListener("DOMContentLoaded", function modal(){
         thridModal.style.display = "none";
       }
     }
-
-
-
-  // Quiz
-  
 });
+
+
+
+// Quiz
+var score = 0; // Set score to 0
+var total = 2; // Total number of questions
+var point = 1; // Points per correct answer
+var highest = total * point;
+
+// Initializer
+function init(){
+  // Set correct answers
+  sessionStorage.setItem('a1', 'c');
+  sessionStorage.setItem('a2', 'b');
+}
+
+  $(document).ready(function() {
+    //Hide all questions
+    $('.questionForm').hide();
+
+    //Show first question
+    $('#q1').show();
+
+    $('#q1 #submit').click(function(){
+      $('.questionForm').hide();
+      process('q1');
+      $('#q2').fadeIn(300);
+      return false;
+  });
+
+  $('#q2 #submit').click(function(){
+      $('.questionForm').hide();
+      process('q2');
+      $('#results').fadeIn(300);
+      return false;
+  });
+});
+
+// Process the answers
+function process(q){
+  if(q == "q1"){
+    var submitted = $('input[name=q1]:checked').val();
+    if(submitted == sessionStorage.a1){
+      score++;
+    }
+  }
+  if(q == "q2"){
+    var submitted = $('input[name=q2]:checked').val();
+    if(submitted == sessionStorage.a2){
+      score++;
+    }
+    $('#results').html('<h3>Ditt resultat är: '+score+' av 2</h3><a href="index.html">Gör quiz igen</a>');
+  }
+  return false;
+}
+
+// Add event listener
+window.addEventListener('load', init, false);
+
+document.getElementById("myCheck").disabled = true;
